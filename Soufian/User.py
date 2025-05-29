3807bcf6c6ffd7ad04c978e21ab2a2a21df5318d
Dein Name <deine.email@beispiel.de>
2025-05-29 23:53:07 +0200

    user

---
+++ Soufian/User.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, handle, ip, port):
+        self.handle = handle  
+        self.ip = ip          
+        self.port = port      
+
+    def __str__(self):
+        return f"User(handle='{self.handle}', ip='{self.ip}', port={self.port})"
