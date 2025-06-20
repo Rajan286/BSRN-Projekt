0f9f65170e1cb4d629fd81bb1a63bbe2cc681f3c
Rajan Kakkar <rajan.kakkar@stud.fra-uas.de>
2025-06-20 16:33:29 +0200

    discovery.py mit load config

---
+++ Projekt/discovery.py
@@ -0,0 +1,22 @@
+import threading
+from common import load_config
+from peer import Peer
+
+FIFO_UI_TO_DISC = "ui_to_discovery"
+FIFO_DISC_TO_UI = "discovery_to_ui"
+
+class Discovery:
+    """
+    Discovery-Dienst, der auf WHO-Nachrichten antwortet
+    und JOIN/LEAVE verarbeitet.
+    """
+    def __init__(self, config_path="config.toml"):
+        self.config = load_config(config_path)
+        self.handle = self.config["handle"]
+        self.port = self.config["port"]
+        self.whois_port = self.config.get("whoisport", 4000)
+        self.known_peers = {}
+        self.known_peers[self.handle] = Peer(self.handle, "255.255.255.255", self.port)
+
+        self.listen_ui_thread = threading.Thread(target=self.listen_to_ui, daemon=True)
+        self.udp_listener_thread = threading.Thread(target=self.listen_udp_all, daemon=True)
